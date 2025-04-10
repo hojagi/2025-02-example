bd3a719d6197922091902989c82545a7143dc512
raguzin <53191356+raguzin@users.noreply.github.com>
2025-04-10 20:49:13 +0300

    lesson20

---
+++ lesson20/kind/kind_create.sh
@@ -0,0 +1,26 @@
+#! /bin/env bash
+export DOCKER_HOST_IP="192.168.56.18"
+# export DOCKER_HOST_EXTERNAL_IP="51.250.78.64"
+export POD_SUBNET="10.240.0.0/16"
+export SERVICE_SUBNET="10.0.0.0/16"
+export VOLUME_HOST_PATH='/tmp/data/'
+export VOLUME_CONTAINER_PATH="/data"
+
+# create kind cluster
+envsubst < config.yml | kind create cluster --config=-
+
+# # add external ip docker-machine into kubeconfig
+# echo "Edit kube config file..."
+# sed -i "s/$DOCKER_HOST_IP/$DOCKER_HOST_EXTERNAL_IP/g" ~/.kube/config
+
+# # get kubernetes cluster info
+# kubectl --insecure-skip-tls-verify cluster-info
+
+# install Calico
+echo "Install Calico CNI..."
+kubectl --insecure-skip-tls-verify apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
+# watch kubectl --insecure-skip-tls-verify get pods -l k8s-app=calico-node -A
+
+# install Nginx ingress controller
+echo "Install Ingress NGINX..."
+kubectl --insecure-skip-tls-verify apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
